2d4d0f7e74fe3521943a22a2c532ee878ca5d4f6
Pratyush Saini <66582958+pratyushiitd@users.noreply.github.com>
2022-03-08 14:38:28 +0530

    -> and .

--- Pointers in C++/main1.cpp
+++ Pointers in C++/main1.cpp
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
+
 struct sema{
     int val;  
 };
-
-void func(int *sema){
-    std::cout << sema->val+1;
+void func(sema *s){
+    printf("%d",s->val+1); //Accessing attrbutes of pointer occurs with ->
 }
-
 int main()
 {
     printf("Hello World");
     sema s;
-    s->val = 6;
-    func(&s);
+    s.val = 6; //Accessing attributes of object / struct occurs with .
+    func(&s); //Pass address of struct sema s to the function func
     return 0;
 }
